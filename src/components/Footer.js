383443671d187aba32f25aaae9a82598f94c937d
Kristian Vassilev <kvasilev@devrix.com>
2021-11-19 18:45:13 +0200

    Created components from initial template

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+	return (
+		<>
+			<footer className="tm-black-bg">
+				<div className="container">
+					<div className="row">
+						<p className="tm-copyright-text">
+							Copyright &copy; 2021 Kristian Vassilev | React Holiday Catalog Project
+						</p>
+					</div>
+				</div>
+			</footer>
+		</>
+	);
+};
+
+export default Footer;

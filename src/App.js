2f138b6743d11c6f21ed9932c72060ed36d30f71
Kristian Vassilev <kvasilev@devrix.com>
2021-11-20 13:55:03 +0200

    Changed componenet strucutre, cleared some html and added css to homepage components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Header from './components/Header';
-import Banner from './components/Banner';
-import Footer from './components/Footer';
-import MostPopularHolidays from './components/MostPopularHolidays';
-import Authors from './components/Authors';
+import Header from './components/Header/Header';
+import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
+import MostPopularHolidays from './components/MostPopularHolidays/MostPopularHolidays';
+import Authors from './components/Authors/Authors';
 
 function App() {
 	return (

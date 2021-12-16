6641fe9fc4a57b3ca7c4988a32fda4dd79b0a158
Kristian Vassilev <kvasilev@devrix.com>
2021-12-16 17:04:57 +0200

    Changed added by me styling

--- src/components/DestinationsByMe/DestinationsByMe.js
+++ src/components/DestinationsByMe/DestinationsByMe.js
@@ -6,7 +6,8 @@ import AuthContext from "../../contexts/AuthContext";
 import * as holidayService from "../../services/holidayService";
 
 import './DestinationsByMe.css';
-import PopularHolidayCard from "../MostPopularHolidays/PopularHolidayCard/PopularHolidayCard";
+import HolidayCard from "../../components/HolidayCatalog/HolidayCard/HolidayCard";
+
 
 const DestinationsByMe = () => {
 	const contextValue = useContext(AuthContext);
@@ -21,20 +22,14 @@ const DestinationsByMe = () => {
 	}, [authorId]);
 
 	return (
-			<section className="tm-white-bg section-padding-bottom">
-				<div className="container">
+			<section className="added-by-me-section">
+				<h2>Created by me:</h2>
 					<div className="row">
-						<div className="tm-section-header section-margin-top">
-							<div className="col-lg-4 col-md-6 col-sm-6">
-								<h2>Destinations By me:</h2>
-							</div>
-						</div>
 							{ownDestinations.length > 0 
 								? ( ownDestinations.map((x) => (
-									<PopularHolidayCard key={x.id} destination={x} /> ))) 
+									<HolidayCard key={x.id} destination={x} /> ))) 
 								: ( <p>No destinations yet</p>)}
 					</div>
-				</div>
 			</section>
 	);
 };

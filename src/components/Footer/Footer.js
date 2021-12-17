bfd9636dbdcdc8df8e822643543f828013e92896
Kristian Vassilev <kvasilev@devrix.com>
2021-12-17 14:49:29 +0200

    Added footer link to repo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import './Footer.css';
 
 const Footer = () => {
@@ -7,7 +8,10 @@ const Footer = () => {
 				<div className="container">
 					<div className="row">
 						<p className="tm-copyright-text">
-							Copyright &copy; 2021 Kristian Vassilev | React Holiday Catalog Project
+							Copyright &copy; 2021 Kristian Vassilev | 
+							<NavLink to={{pathname: "https://github.com/k-vassilev/react-holiday-catalog"}} target="_blank">
+								React Holiday Catalog Project
+							</NavLink>
 						</p>
 					</div>
 				</div>

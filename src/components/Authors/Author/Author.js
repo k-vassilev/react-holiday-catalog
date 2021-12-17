242195d6b4091629955ebb29e729f0e4b8509a0d
Kristian Vassilev <kvasilev@devrix.com>
2021-12-17 15:21:14 +0200

    Added default user image if no image url is provided

--- src/components/Authors/Author/Author.js
+++ src/components/Authors/Author/Author.js
@@ -5,7 +5,12 @@ const Author = (props) => {
 		<>
 			<div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
 				<div className="tm-about-box-1">
-					<img src={props.author.acf.image_url} alt="Author" className="tm-about-box-1-img author-img"/>
+					<img 
+					src={props.author.acf.image_url} 
+					alt="Author" 
+					onError={(e)=>{e.target.onerror = null; 
+					e.target.src="https://www.pngitem.com/pimgs/m/294-2947257_interface-icons-user-avatar-profile-user-avatar-png.png"}} 
+					className="tm-about-box-1-img author-img"/>
 					<h3 className="tm-about-box-1-title">{props.author.acf.username}</h3>
 				</div>
 			</div>
